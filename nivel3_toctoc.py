8ebff600782d8b8af65c18f3cde0801ed89dc03b
Seba Mena <smenaaliaga@gmail.com>
2022-04-16 22:09:44 -0400

    Upgrade scraping code to Toctoc with Selenium

--- nivel3_toctoc.py
+++ nivel3_toctoc.py
@@ -1,8 +1,8 @@
-import random
-from time import sleep
-from tkinter import E
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -12,20 +12,22 @@ driver.get('https://www.toctoc.com/arriendo/departamento/metropolitana/santiago'
 # Obtener todos los departamentos
 deptos = driver.find_elements_by_xpath('//li[@class="un-ress tp3"]')
 
-# Crear boton de carga
-btn = driver.find_element_by_xpath('//button[@id="btnCargarMas"]')
-
 for i in range(3) :
     try: 
+        # Instanciar boton de carga una vez exista
+        btn = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//button[@id="btnCargarMas"]'))
+        )
         # Click a boton de carga
-        btn.click()
-        # Recrear boton de carga
-        btn = driver.find_element_by_xpath('//button[@id="btnCargarMas"]')
-        # Espera para cargar más data
-        sleep(random.uniform(8,10))
+        btn.click() 
+        # Esperar que se cargue la información cargada
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//li[@class="un-ress tp3"]//h3[@class="dir etc"]'))
+        )
     except :
         break
 
+# Recorrer cada uno de los departamentos obtenidos
 for depto in deptos :
     titulo = depto.find_element_by_xpath('.//h3[@class="dir etc"]').text
     print(titulo)
